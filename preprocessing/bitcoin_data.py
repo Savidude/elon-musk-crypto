a3de4aaff70de69f302ac545873258f5685212e9
Savidude <mysticsavy@gmail.com>
2022-04-29 11:01:30 +0300

    Analyse empty values in the bitcoin dataset

---
+++ preprocessing/bitcoin_data.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+spark_session = SparkSession.builder \
+        .master("local") \
+        .appName("Elon Musk Crypto") \
+        .getOrCreate()
+
+bitcoin_data = spark_session.read.csv("../data/bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv",
+                                 header=True, inferSchema=True)
+bitcoin = bitcoin_data.toPandas()
+
+# plt.figure(figsize = (25, 11))
+sns.heatmap(bitcoin.isna().values[::10], cmap = ["#64b5f6", "#e57373"], xticklabels=bitcoin.columns)
+plt.title("Bitcoin data missing values", size=20)
+plt.show()
+
+"""
+Visualise the large amount of missing values in bitcoin the dataset
+"""
